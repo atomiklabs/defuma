85aeaed1279e0237e0fc43e3a85d199fb1856885
deployAt <mikowhy@tutanota.com>
2020-07-20 22:49:07 +0200

    test: defuma providers & ipns

--- packages/actors/contracts/Defuma.sol
+++ packages/actors/contracts/Defuma.sol
@@ -9,14 +9,14 @@ contract Defuma {
 
     mapping(uint256 => address) public providers;
     uint256 public providersCount;
-    mapping(address => string) public productIpfs;
+    mapping(address => string) public providerData;
 
-    function addProvider(address providerAddress) public {
+    function registerProvider(address providerAddress) public {
         providersCount = providersCount.add(1);
         providers[providersCount] = providerAddress;
     }
 
-    function addProduct(address providerAddress, string memory hash) public {
-        productIpfs[providerAddress] = hash;
+    function addProviderData(address providerAddress, string memory hash) public {
+        providerData[providerAddress] = hash;
     }
 }
