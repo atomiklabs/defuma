eb4ad214cb66ce4488de45e2821c46ccf2494768
deployAt <mikowhy@tutanota.com>
2020-07-24 11:34:44 +0200

    feat: authehtication

--- packages/web-ui/src/publisher/publisher.tsx
+++ packages/web-ui/src/publisher/publisher.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { Balance } from './balance'
 import { Defuma } from './defuma'
+import { Login } from '../login'
 
 export const Publisher: FC = () => {
   const { chainId, account, active } = useWeb3React()
@@ -9,6 +10,7 @@ export const Publisher: FC = () => {
   if (!active) return <h3>Loading...</h3>
   return (
     <div>
+      <Login />
       <p>ChainId: {chainId}</p>
       <p>Account: {account}</p>
       <Balance />
