0dca9e3ce438b1afb680ea31535441f1621d1cd5
Tomasz Kopacki <tomasz@kopacki.net>
2020-07-26 17:19:31 +1000

    feat(buckets): integrate with web-ui

--- packages/web-ui/src/index.tsx
+++ packages/web-ui/src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Main } from './main'
+import { ContentManager } from '@atomiklabs/content-manager'
 
 ReactDOM.render(
   <React.StrictMode>
+    <ContentManager userApiKey="bxikmslquguiw6zcmmcm7glzvtm" userApiSecret="" bucketName="files-miki" />
     <Main />
   </React.StrictMode>,
   document.getElementById('root')
